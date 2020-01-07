87228febfb8ac1824ae365835752a6df7a028fc8
David Guzman <d.guzman@ucl.ac.uk>
2020-01-07 14:47:03 +0000

    Babel configuration, update Next

---
+++ pages/index.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Index from './index';
+
+describe('Index component', () => {
+
+  it('Shallow mount Index', () => {
+    const index = shallow(<Index />);
+    expect(index.find('div').text()).toEqual('TEMPLATE');
+  });
+
+});
