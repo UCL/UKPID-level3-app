6f589d0425fa0c3646325d7d41fa06d1d97d2151
David Guzman <d.guzman@ucl.ac.uk>
2020-03-04 16:38:39 +0000

    Make select in question4 a controlled component

--- components/trainee-network/question4.js
+++ components/trainee-network/question4.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import FormControl from '@material-ui/core/FormControl';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -12,16 +12,22 @@ const question4Text = 'What was the primary indication for the use of the '
 
 const Question4Prompt = () => <Typography>{question4Text}</Typography>;
 
-const Question4Data = () => (
-  <FormControl>
-    <Select value="asthma">
-      {
-        q4MenuData.map((item) => (
-          <MenuItem key={item.id} value={item.value}>{item.label}</MenuItem>
-        ))
-      }
-    </Select>
-  </FormControl>
-);
+const Question4Data = () => {
+  const [indication, setIndication] = useState('');
+  return (
+    <FormControl>
+      <Select
+        value={indication}
+        onChange={(event) => setIndication(event.target.value)}
+      >
+        {
+          q4MenuData.map((item) => (
+            <MenuItem key={item.id} value={item.value}>{item.label}</MenuItem>
+          ))
+        }
+      </Select>
+    </FormControl>
+  )
+};
 
 export { Question4Prompt, Question4Data };
