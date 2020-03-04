6f1c3b74ecee925b9e6b93338d2d65fd0681804f
David Guzman <d.guzman@ucl.ac.uk>
2020-03-04 14:27:17 +0000

    Convert menu items in question 3 to be populated dynamically

--- components/trainee-network/question3.js
+++ components/trainee-network/question3.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
@@ -12,17 +13,45 @@ const question3Text = 'Which biological agent has been used?';
 
 const Question3Prompt = () => <Typography>{question3Text}</Typography>;
 
-const Question3Data = () => (
-  <FormControl>
-    <Select value="anti-tnfs">
-    {
-      q3_menu_data.map(item => (
-        <MenuItem key={item.id} value={item.value}>{item.label}</MenuItem>
-      ))
-    }
-    </Select>
-    <FormHelperText>Class of medication</FormHelperText>
-  </FormControl>
-);
+const Question3Data = () => {
+  const [ classofmed, setClassofmed ] = useState('');
+  const [ biologic, setBiologic ] = useState('');
+  return (
+    <FormGroup>
+      <FormControl>
+        <Select
+          value={classofmed}
+          onChange={event => {
+            setClassofmed(event.target.value);
+            setBiologic('');
+          }}
+          >
+          {
+            q3_menu_data.map(item => (
+              <MenuItem key={item.id} value={item.value}>{item.label}</MenuItem>
+            ))
+          }
+        </Select>
+        <FormHelperText>Class of medication</FormHelperText>
+      </FormControl>
+      <FormControl>
+        <Select
+          value={biologic}
+          onChange={event => setBiologic(event.target.value)}
+        >
+          {
+            q3_menu_data.filter(
+              item => item.value === classofmed
+            ).flatMap(
+              element => element.biologics
+            ).map(bio => (
+              <MenuItem key={bio.id} value={bio.value}>{bio.label}</MenuItem>
+            ))
+          }
+        </Select>
+      </FormControl>
+    </FormGroup>
+  );
+};
 
 export { Question3Data, Question3Prompt };
